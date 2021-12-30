674ad23344d62ec134c429f898864a41695f85c4
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2021-12-29 20:10:26 -0600

    Add project files.

---
+++ GaussJordanElimination_double2x2.cs
@@ -0,0 +1,31 @@
+﻿using FrooxEngine;
+using FrooxEngine.LogiX;
+using BaseX;
+using Mehroz;
+
+namespace MatrixMod
+{
+    [NodeName("Reduced Echelon Form")]
+    [NodeOverload("Reduced-Echelon-Form")]
+    [Category(new string[] { "LogiX/Math/Matrix" })]
+
+    public sealed class GaussJordanElimination: LogixNode
+    {
+        public readonly Input<double2x2> LinearEquationMatrix;
+        public readonly Input<double2> LinearSolutionMatrix;
+        public readonly Output<double2> SolutionMatrix;
+
+        // Code goes here!
+        protected override void OnEvaluate()
+        {
+            Matrix m1 = new Matrix(LinearEquationMatrix.EvaluateRaw().To2DArray());
+            Matrix m2 = new Matrix(2, 1);
+            m2[0, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().x);
+            m2[1, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().y);
+            Matrix m3 = Matrix.Concatenate(m1, m2);
+            m3 = m3.ReducedEchelonForm();
+            SolutionMatrix.Value = new double2(m3[0, 2].ToDouble(), m3[1, 2].ToDouble());
+        }
+    }
+}
+
---
+++ GaussJordanElimination_double3x3.cs
@@ -0,0 +1,32 @@
+﻿using FrooxEngine;
+using FrooxEngine.LogiX;
+using BaseX;
+using Mehroz;
+
+namespace MatrixMod
+{
+    [NodeName("Reduced Echelon Form")]
+    [NodeOverload("GaussJordanElimination")]
+    [Category(new string[] { "LogiX/Math/Matrix" })]
+
+    public sealed class GaussJordanElimination_double3x3 : LogixNode
+    {
+        public readonly Input<double3x3> LinearEquationMatrix;
+        public readonly Input<double3> LinearSolutionMatrix;
+        public readonly Output<double3> SolutionMatrix;
+
+        // Code goes here!
+        protected override void OnEvaluate()
+        {
+            Matrix m1 = new Matrix(LinearEquationMatrix.EvaluateRaw().To2DArray());
+            Matrix m2 = new Matrix(3, 1);
+            m2[0, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().x);
+            m2[1, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().y);
+            m2[2, 0] = new Fraction(LinearSolutionMatrix.EvaluateRaw().z);
+            Matrix m3 = Matrix.Concatenate(m1, m2);
+            m3 = m3.ReducedEchelonForm();
+            SolutionMatrix.Value = new double3(m3[0, 2].ToDouble(), m3[1, 2].ToDouble(), m3[2, 2].ToDouble());
+        }
+    }
+}
+
